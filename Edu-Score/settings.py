2dddcf3ee0366775c667b02343c07c68acfb7ca4
Mayur Mahajan <mahajanmayur2111@gmail.com>
2025-11-13 00:50:28 +0530

    Clean minimal requirements for Vercel

--- Edu-Score/settings.py
+++ Edu-Score/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-0w01*qjx6xb8$^)5mu^zp9#$(v(fo457%dkx^#q$47y7-*jox6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '*', ".vercel.app"]
 
 
 
