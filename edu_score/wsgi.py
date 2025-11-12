6cc72018214f7ea4e2250b3157666146eb19075f
Mayur Mahajan <mahajanmayur2111@gmail.com>
2025-11-13 01:09:08 +0530

    Add error logging + fix envs

--- edu_score/wsgi.py
+++ edu_score/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Digitalilm.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'edu_score.settings')
 
 application = get_wsgi_application()
